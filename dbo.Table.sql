cca9a1ab1de032efce9490652a5eb33d2744d5c7
Alexandre <jonieca15@gmail.com>
2021-01-18 11:55:45 +0100

    Ajout bdd

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [mail] NVARCHAR(MAX) NOT NULL, 
+    [password] NVARCHAR(MAX) NOT NULL, 
+    [creationDate] DATETIME NOT NULL
+)
